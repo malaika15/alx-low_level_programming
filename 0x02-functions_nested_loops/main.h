30cf9710a081e8f56716a2cd35dd1d8b0db393fd
malaika15 <nguedjitanhermine@gmail.com>
2023-03-21 09:16:10 -0700

    print_putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void print_to_98(int n);
+int _islower(int c);
+int _isalpha(int c);
+void jack_bauer(void);
+void times_table(void);
+void print_times_table(int n);
+
+
+#endif
